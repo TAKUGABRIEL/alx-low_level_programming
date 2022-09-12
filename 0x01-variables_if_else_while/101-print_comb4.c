3754d13906881efc88c3dde8139fe203b3ff8aa3
TAKUGABRIEL <takugabriel.tg@gmail.com>
2022-09-12 04:06:03 -0700

    fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,40 @@
+/*
+ * File: 101-print_comb4.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
-* main - Print combinations of two digit numbers
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int tens;
-	int ones;
-	int hunds;
+	int digit1, digit2, digit3;
 
-	for (tens = 0; tens <= 9; tens++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (ones = tens + 1; <= 9; ones++)
-			for (hunds = ones + 1; <= 9; hunds++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				putchar(tens + '0');
-				putchar(ones + '0');
-				putchar(hunds + '0');
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
 
-				if (tens < 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+				
+				putchar(',');
+				putchar(' ');
 			}
+		}
 	}
+
 	putchar('\n');
 
 	return (0);
 }
-
